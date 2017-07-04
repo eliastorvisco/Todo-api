bf2d6e5a0a26a677da3552dba016d421fde95ec7
Elias Torvisco <eliastorvisco.macia@gmail.com>
2017-07-04 13:42:16 +0200

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-var express = require('express');
-var app = express();
+var express = require('express'); //Allows us do GETs and POSTs
+var bodyParser = require('body-parser'); //To send data with POSTs
+
+var app = express(); //Used to do GETs and POSTs
 var PORT = process.env.PORT || 3000; //So if it is running in Heroku it works!
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Finish exams',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json()); //middleware for everytime json request comes in
+                            //express is gonna parse it and we can access it
+                            //via request.body
 
 app.get('/', function (request, response) {
     response.send('Todo API Root');
@@ -38,8 +33,16 @@ app.get('/todos/:id', function (request, response) {
     //response.send('Asking for todo with id of ' + request.params.id);
     if (matchedTodo) response.json(matchedTodo);
     else response.status(404).send();
+});
 
-})
+//POST  /todos
+//npm install body-parser@1.13.3 --save
+app.post('/todos', function (request, response) {
+    var body = request.body;
+    body.id = todoNextId++;
+    todos.push(body);
+    response.json(body);
+});
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');

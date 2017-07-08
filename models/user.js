5f7a464c590421eef059fa68deacc4bb92f62021
Elias Torvisco <eliastorvisco.macia@gmail.com>
2017-07-08 18:19:36 +0200

    Convert email to lower case

--- models/user.js
+++ models/user.js
@@ -15,5 +15,14 @@ module.exports = function (sequelize, DataTypes) {
                     len: [7, 100]
                 }
             }
-        });
+        }, {
+            hooks: {
+                beforeValidate: function (user, options) {
+                    //So heLLo@gmail.com = Hello@gmail.com
+                    if (typeof user.email === 'string') {
+                        user.email = user.email.toLowerCase();
+                    }
+                }
+            }
+    });
 }

029e6b0dad0cf9fe9cf1b7fcb2bd5ffd1bf1979d
Elias Torvisco <eliastorvisco.macia@gmail.com>
2017-07-08 17:48:03 +0200

    Convert PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -89,28 +89,30 @@ app.delete('/todos/:id', function (request, response) {
 //PUT /todos/:id  - (Updates)
 app.put('/todos/:id', function (request, response) {
     var todoId = parseInt(request.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
     var body = _.pick(request.body, 'description', 'completed');
-    var validAttributes = {};
+    var attributes = {};
 
-    if(!matchedTodo) {
-        return response.status(404).send();
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
     }
 
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        return response.status(400).send();
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
     }
 
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return response.status(400).send();
-    }
-
-    _.extend(matchedTodo, validAttributes);
-    response.json(matchedTodo);
+    db.todo.findById(todoId).then(function (todo) {
+        if (todo) {
+            todo.update(attributes).then(function (todo) {
+                response.json(todo.toJSON());
+            }, function (error) {
+                response.status(400).json(error);
+            });
+        } else {
+            response.status(404).send();
+        }
+    }, function () {
+        response.status(500).send();
+    });
 });
 
 db.sequelize.sync().then(function () {

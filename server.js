43ee8dcbed5ed3731c664913ab6e94b4e741a2db
Elias Torvisco <eliastorvisco.macia@gmail.com>
2017-07-11 13:56:16 +0200

    Force new creation of database

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ app.post('/users/login', function (request, response) {
 
 });
 
-db.sequelize.sync().then(function () {
+db.sequelize.sync({force: true}).then(function () {
     app.listen(PORT, function () {
         console.log('Express listening on port ' + PORT + '!');
     });

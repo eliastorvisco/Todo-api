ddfe9f86ab306ce4641c4763f722d725c4767f9e
Elias Torvisco <eliastorvisco.macia@gmail.com>
2017-07-11 21:35:41 +0200

    Adding DELETE /users/login

--- server.js
+++ server.js
@@ -147,17 +147,29 @@ app.post('/users', function(request, response) {
 //POST /users/login
 app.post('/users/login', function (request, response) {
     var body = _.pick(request.body, 'email', 'password');
+    var userInstance;
     db.user.authenticate(body).then(function (user) {
         var token = user.generateToken('authentication');
-        if (token) {
-            response.header('Auth', token).json(user.toPublicJSON());
-        } else {
-            response.status(401).send();
-        }
-    }, function () {
+        userInstance = user;
+        return db.token.create({
+            token: token
+        });
+    }).then(function (tokenInstance) {
+        response.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+    }).catch(function (error) {
+        console.log(error);
         response.status(401).send();
     });
 
+});
+
+//DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (request, response) {
+    request.token.destroy().then(function() {
+        response.status(204).send();
+    }).catch(function () {
+        response.status(500).send();
+    });
 });
 
 db.sequelize.sync({force: true}).then(function () {
